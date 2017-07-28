6a7a5399d1ce3fc3ddc317395c54ec537b299a2b
Sylvain Simao <Sylvain.Simao@clemenger.com.au>
2017-07-28 15:40:53 +1000

    update build location

--- build/config/index.js
+++ build/config/index.js
@@ -21,8 +21,8 @@ module.exports = {
     template: assign({ env: envParams }, defaultBaseLocale),
     build: {
         env: require('./prod.env'),
-        index: path.resolve(__dirname, '../dist/index.html'),
-        assetsRoot: path.resolve(__dirname, '../dist'),
+        index: path.resolve(__dirname, '../../dist/index.html'),
+        assetsRoot: path.resolve(__dirname, '../../dist'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
         productionSourceMap: true,
